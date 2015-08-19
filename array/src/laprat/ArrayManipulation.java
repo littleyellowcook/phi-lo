0dcc0fe5a3d699ffe72bd87a6acaeb0daf56f753
Philomena Taig <taig@adtech.com>
2015-08-19 18:22:59 +0100

    first commit

---
+++ array/src/laprat/ArrayManipulation.java
@@ -0,0 +1,47 @@
+package laprat;
+
+import java.util.ArrayList;
+
+/**
+ * Class use for ArrayList manipulations
+ * @author philotaig
+ *
+ */
+public class ArrayManipulation {
+
+	/**
+	 * Return an ArrayList of given list of objects
+	 * @param objects
+	 * @return
+	 */
+	public ArrayList<Object> createArray(Object...objects) {
+		ArrayList<Object> result = new ArrayList<Object>();
+		for (Object obj : objects){
+			result.add(obj);
+		}
+		return result;
+	}
+
+	/**
+	 * Flatten given (nested) ArrayList into a single dimension ArrayList  
+	 * @param testArray
+	 * @return
+	 */
+	public ArrayList<Object> flattenArray(ArrayList<Object> testArray) {
+		ArrayList<Object> result = new ArrayList<Object>();
+		processArray(testArray, result);
+		return result;
+	}
+
+	@SuppressWarnings("unchecked")
+	private void processArray(ArrayList<Object> testArray,
+			ArrayList<Object> result) {
+		for (Object obj : testArray){
+			try {
+				processArray((ArrayList<Object>)obj, result);
+			} catch (Exception e) {
+				result.add(obj);
+			}
+		}
+	}
+}
---
+++ array/src/laprat/ArrayManipulationTest.java
@@ -0,0 +1,77 @@
+package laprat;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ArrayManipulationTest {
+	
+	public ArrayManipulation am;
+
+	@Before
+	public void setUp() throws Exception {
+		am = new ArrayManipulation();
+	}
+
+	@Test
+	public void testCreateSimpleArray() {
+		ArrayList<Object> actual = am.createArray("a");
+		ArrayList<Object> expected = new ArrayList<Object>(Arrays.asList("a"));
+		assertArrayEquals("create simple array failed", expected.toArray(), actual.toArray());
+	}
+	
+	@Test
+	public void testCreateMixArray(){
+		ArrayList<Object> actual = am.createArray("a", 1.2, 500L, false);
+		ArrayList<Object> expected = new ArrayList<Object>(Arrays.asList("a", 1.2, 500L, false));
+		assertArrayEquals("create mix array failed", expected.toArray(), actual.toArray());
+	}
+	
+	@Test
+	public void testNestedArray(){
+		ArrayList<Object> subarray = am.createArray("a", "b", 1, 10L, true);
+		ArrayList<Object> actual = am.createArray("a", 1.2, 500L, false, subarray);
+		ArrayList<Object> expected = new ArrayList<Object>(Arrays.asList("a", 1.2, 500L, false, subarray));
+		assertArrayEquals("create nexted array failed", expected.toArray(), actual.toArray());
+	}
+	
+	@Test
+	public void testFlatten1dArray(){
+		ArrayList<Object> testArray = am.createArray("a", "b", 1, 10L, true);
+		ArrayList<Object> actual = am.flattenArray(testArray);
+		ArrayList<Object> expected = new ArrayList<Object>(Arrays.asList("a", "b", 1, 10L, true));
+		assertArrayEquals("flatten 1d array failed", expected.toArray(), actual.toArray());
+	}
+	
+	@Test
+	public void testFlatten2dArray(){
+		ArrayList<Object> subArray = am.createArray("a", "b", 1, 10L, true);
+		ArrayList<Object> testArray = am.createArray("a", subArray, 1.2, 500L, false, subArray);
+		ArrayList<Object> actual = am.flattenArray(testArray);
+		ArrayList<Object> expected = new ArrayList<Object>(Arrays.asList("a", "a", "b", 1, 10L, true, 1.2, 500L, false, "a", "b", 1, 10L, true));
+		assertArrayEquals("flatten 2d array failed", expected.toArray(), actual.toArray());
+	}
+	
+	@Test
+	public void testFlatten3dArray(){
+		ArrayList<Object> secondLevelArray = am.createArray("a", "b", 1, 10L, true);
+		ArrayList<Object> firstLevelArray = am.createArray(4, false, 5L, "d", secondLevelArray);
+		ArrayList<Object> testArray = am.createArray("a", firstLevelArray, 1.2, 500L, false);
+		ArrayList<Object> actual = am.flattenArray(testArray);
+		ArrayList<Object> expected = new ArrayList<Object>(Arrays.asList("a", 4, false, 5L, "d", "a", "b", 1, 10L, true, 1.2, 500L, false));
+		assertArrayEquals("flatten 3d array failed", expected.toArray(), actual.toArray());
+	}
+	
+	@Test
+	public void testDifferentDataType(){
+		String[] stringArray = {"a", "b", "c"};
+		ArrayList<Object> testArray = am.createArray("a", stringArray, 1.2, 500L, false);
+		ArrayList<Object> actual = am.flattenArray(testArray);
+		ArrayList<Object> expected = new ArrayList<Object>(Arrays.asList("a", "a", "b", "c", "b", 1, 10L, true));
+		assertArrayEquals("flatten different datatype failed", expected.toArray(), actual.toArray());
+	}
+}
